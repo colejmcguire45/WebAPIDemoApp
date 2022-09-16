e6b74aae6152215a731d99d1adcc2de7fb570a28
Cole McGuire <ColeMcGuire@paulconsulting.com>
2022-09-16 10:38:18 -0400

    Adds people controller with get functionality

---
+++ DemoAPI/DemoAPI/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Cole", LastName = "McGuire", Id = 1 });
+            people.Add(new Person { FirstName = "John", LastName = "Doe", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Baggins", Id = 3 });
+            people.Add(new Person { FirstName = "Mike", LastName = "Honcho", Id = 4 });
+        }
+
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
